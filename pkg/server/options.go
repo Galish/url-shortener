20e70ac3663b0c617c416f465d3717be30aa2ff4
Ilshat Galin <iamilshatgalin@gmail.com>
2024-02-20 12:05:32 +0500

    Added godoc comments

--- pkg/server/options.go
+++ pkg/server/options.go
@@ -1,14 +1,17 @@
 package server
 
+// Option updates the setting in the server configuration.
 type Option func(*Server)
 
+// WithAddress updates the server address setting.
 func WithAddress(addr string) Option {
 	return func(s *Server) {
 		s.addr = addr
 	}
 }
 
-func WithSecureTransport(cnf *TLSConfig) Option {
+// WithTLS updates the server TLS settings.
+func WithTLS(cnf *TLSConfig) Option {
 	return func(s *Server) {
 		s.tlsConfig = cnf
 	}
