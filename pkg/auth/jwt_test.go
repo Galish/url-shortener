22160cabc6c6a1b627867436c41d5359418154e9
Ilshat Galin <iamilshatgalin@gmail.com>
2024-02-26 11:47:31 +0500

    JWT utilities covered with tests

--- pkg/auth/jwt_test.go
+++ pkg/auth/jwt_test.go
@@ -2,7 +2,81 @@ package auth
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func TestNewToken(t *testing.T) {
+	tests := []struct {
+		name   string
+		claims *JWTClaims
+		hasErr bool
+		want   string
+	}{
+		{
+			"blank claims",
+			&JWTClaims{},
+			false,
+			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiIifQ.nucTpEFKTGXpePK8UYy7GGVwmiYd29l86EMB-_8UQ28",
+		},
+		{
+			"claims containing user identifier",
+			&JWTClaims{
+				UserID: "#12345",
+			},
+			false,
+			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiIjMTIzNDUifQ.VsXLWQqA8jbGie95Oj58X0wObuiYcN-3qitptUUwNjQ",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			token, err := NewToken(tt.claims)
+
+			assert.Equal(t, tt.hasErr, err != nil)
+			assert.Equal(t, tt.want, token)
+		})
+	}
+}
+
+func TestParseToken(t *testing.T) {
+	tests := []struct {
+		name   string
+		token  string
+		hasErr bool
+		want   *JWTClaims
+	}{
+		{
+			"blank claims",
+			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiIifQ.nucTpEFKTGXpePK8UYy7GGVwmiYd29l86EMB-_8UQ28",
+			false,
+			&JWTClaims{},
+		},
+		{
+			"claims containing user identifier",
+			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOiIjMTIzNDUifQ.VsXLWQqA8jbGie95Oj58X0wObuiYcN-3qitptUUwNjQ",
+			false,
+			&JWTClaims{
+				UserID: "#12345",
+			},
+		},
+		{
+			"claims containing user identifier",
+			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+			true,
+			nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			token, err := ParseToken(tt.token)
+
+			assert.Equal(t, tt.hasErr, err != nil)
+			assert.Equal(t, tt.want, token)
+		})
+	}
+}
 
 func BenchmarkGenerateToken(b *testing.B) {
 	for i := 0; i < b.N; i++ {
